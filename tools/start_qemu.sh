f5eb5fdd24b4cfac3537698847ada94d873732e5
Daniel Bittman <danielbittman1@gmail.com>
2020-08-19 18:13:09 -0700

    Add tap starting.

--- tools/start_qemu.sh
+++ tools/start_qemu.sh
@@ -14,11 +14,14 @@ instance=0
 for i in $INSTANCES; do 
 	instance=$((instance + 1))
 	echo $instance $i
-	IFS=':' read -a cmd <<< "$i"
+	IFS=',' read -a cmd <<< "$i"
 	waittime="0"
 	NET=""
+	mac=""
+	echo $cmd
 	case ${cmd[0]} in 
-		s)
+		's')
+			mac=${cmd[3]}
 			if [[ "${cmd[1]}" == "l" ]]; then
 				NET="-netdev socket,id=net0,listen=:${cmd[2]}"
 				waittime="0.5"
@@ -26,19 +29,20 @@ for i in $INSTANCES; do
 				NET="-netdev socket,id=net0,connect=:${cmd[2]}"
 			fi
 		;;
-		t)
-			if [[ "${cmd[1]}" == "l" ]]; then
-				NET="-netdev socket,id=net0,listen=:${cmd[2]}"
-				waittime="0.5"
-			else
-				NET="-netdev socket,id=net0,connect=:${cmd[2]}"
-			fi
+		't')
+			mac=${cmd[2]}
+			NET="-netdev tap,id=net0,ifname=${cmd[1]},script=no,downscript=no,vnet_hdr=off"
 		;;
 	esac
-	NETFLAGS="$NET -device e1000e,netdev=net0"
-	echo $NETFLAGS
-	echo $QEMU
-	echo $QEMU_FLAGS
+	if [[ "$mac" == "" ]]; then
+		NETFLAGS="$NET -device e1000e,netdev=net0"
+	else
+		NETFLAGS="$NET -device e1000e,netdev=net0,mac=$mac"
+	fi
+	#echo $NETFLAGS
+	#echo $QEMU
+	#echo $QEMU_FLAGS
+
 	$QEMU -enable-kvm -cdrom $BUILDDIR/boot.iso -serial file:twz_serial_$instance -drive file=$BUILDDIR/us/nvme.img,if=none,id=D22 -device nvme,drive=D22,serial=1234,share-rw=on $NETFLAGS $QEMU_FLAGS &
 	pids+=" $!"
 	sleep $waittime
