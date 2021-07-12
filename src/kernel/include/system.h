4f723bde4b8b49f8b4bae6fef268ec99a9abbe9c
Daniel Bittman <danielbittman1@gmail.com>
2021-07-12 16:57:51 -0700

    document system.h.

--- src/kernel/include/system.h
+++ src/kernel/include/system.h
@@ -1,5 +1,11 @@
 #pragma once
 
+/** @file
+ * @brief Some general utility functions.
+ *
+ * This file is always included.
+ */
+
 #ifndef ASSEMBLY
 
 #include <err.h>
@@ -14,14 +20,7 @@
 typedef long ssize_t;
 typedef unsigned __int128 uint128_t;
 
-/*
-typedef uint128_t objid_t;
-
-#define IDFMT "%lx:%lx"
-
-#define IDPR(id) \
-    (uint64_t)((id) >> 64), (uint64_t)(id & 0xFFFFFFFFFFFFFFFF)
-*/
+/** Round up a value to the nearest power of 2 */
 static inline unsigned long long __round_up_pow2(unsigned int a)
 {
 	return ((a & (a - 1)) == 0) ? a : 1ull << (sizeof(a) * 8 - __builtin_clz(a));
@@ -43,10 +42,25 @@ static inline unsigned long long __round_up_pow2(unsigned int a)
 #define __orderedbefore(x) (x - 1)
 #define __orderedafter(x) (x + 1)
 
+/** Declare a function as a global constructor (and will be executed when the kernel runs global
+ * constructors, see \ref kernelinit). The order of this global constructor relative to others is
+ * not defined. */
 #define __initializer __attribute__((used, constructor, no_instrument_function))
+
+/** Similar to \ref __initializer, but defines an order. Functions registered with a smaller number
+ * 'x' will run first. Functions registered with the same value of 'x' will not have a defined
+ * order.
+ *
+ * Generally, the system will declare a number of major ordering values that can be used. For
+ * example, say there is a constructor that must run after the ACPI tables are setup. The system
+ * might define a ACPI_INITIALIZER_ORDER define so that other functions could be declared with
+ *     __orderedinitializer(__orderedafter(ACPI_INITIALIZER_ORDER)) static void _init() {...}
+ * and thus will run after the ACPI_INITIALIZER_ORDER point. If a function needs to be run after
+ * multiple of these points, they can be summed:
+ *     __orderedafter(ACPI_INITIALIZER_ORDER + IOAPIC_INITIALIZED_ORDER)
+ */
 #define __orderedinitializer(x) __attribute__((used, constructor(x + 3000), no_instrument_function))
 
-#define __cleanup(f) __attribute__((cleanup(f)))
 #define ___concat(x, y) x##y
 #define __concat(x, y) ___concat(x, y)
 
@@ -71,5 +85,6 @@ static inline unsigned long long __round_up_pow2(unsigned int a)
 #define READ 0
 #define WRITE 1
 
+/** See the C library function */
 long strtol(char *str, char **end, int base);
 #endif
