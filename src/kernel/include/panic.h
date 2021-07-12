6173b27769a8a566988cd03b4d2c3753d6842e3a
Daniel Bittman <danielbittman1@gmail.com>
2021-07-12 16:28:24 -0700

    document pmap.h.

--- src/kernel/include/panic.h
+++ src/kernel/include/panic.h
@@ -1,16 +1,23 @@
 #pragma once
 #define PANIC_UNWIND 0x1
 #define PANIC_CONTINUE 0x2
+
+/** Panic. Stops the world, prints debug messages, and halts the system. If flags sets PANIC_UNWIND,
+ * print a stack trace. If flags sets PANIC_CONTINUE, then do not halt the system, and continue
+ * after printing messages. */
 __attribute__((format(printf, 4, 5))) void __panic(const char *file,
   int linenr,
   int flags,
   const char *msg,
   ...);
 
+/** Standard panic. Prints stack trace and stops the world. */
 #define panic(msg, ...)                                                                            \
 	({                                                                                             \
 		__panic(__FILE__, __LINE__, PANIC_UNWIND, msg, ##__VA_ARGS__);                             \
 		__builtin_unreachable();                                                                   \
 	})
+
+/** Continue panic. Prints stack trace and continues execution. */
 #define panic_continue(msg, ...)                                                                   \
 	__panic(__FILE__, __LINE__, PANIC_UNWIND | PANIC_CONTINUE, msg, ##__VA_ARGS__)
--- src/kernel/include/pmap.h
+++ src/kernel/include/pmap.h
@@ -2,11 +2,26 @@
 
 #include <page.h>
 
+/** @file
+ * @brief Permanent physical memory mapping management.
+ *
+ * This is used to map physical memory into virtual memory in a long-lasting, sharable way. If you
+ * just need to map in some pages temporarily, use \ref include/tmpmap.h instead.
+ */
+
 #define PMAP_WB PAGE_CACHE_WB
 #define PMAP_UC PAGE_CACHE_UC
 #define PMAP_WT PAGE_CACHE_WT
 #define PMAP_WC PAGE_CACHE_WC
 
-void *pmap_allocate(uintptr_t phys, size_t, int flags);
+/** Allocate some virtual region that maps to physical memory.
+ * @param phys the physical memory that we will map to. Does not need to be page aligned.
+ * @param len length of the mapping. does not need to be page aligned.
+ * @param flags. Caching flags, one of PMAP_*.
+ * @return a virtual memory pointer that will be mapped to the start of physical memory referred to
+ * by phys. This function may map more memory than requested to satisfy internal alignment
+ * requirements.
+ */
+void *pmap_allocate(uintptr_t phys, size_t len, int flags);
 struct memory_stats;
 void pmap_collect_stats(struct memory_stats *stats);
