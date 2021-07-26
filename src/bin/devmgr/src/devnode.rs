1291e320f8969a1cc7f8b8b6af8c06679661df65
Daniel Bittman <danielbittman1@gmail.com>
2021-07-26 13:01:25 -0700

    Add device node name management.

---
+++ src/bin/devmgr/src/devnode.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+use std::{lazy::SyncLazy, sync::Mutex};
+use twz::obj::ObjID;
+
+pub struct DeviceNode {
+	pub name: String,
+	pub id: ObjID,
+}
+
+struct DeviceNodeState {
+	rootcounters: HashMap<String, u32>,
+}
+
+impl DeviceNodeState {
+	fn new() -> DeviceNodeState {
+		DeviceNodeState {
+			rootcounters: HashMap::new(),
+		}
+	}
+}
+
+static STATE: SyncLazy<Mutex<DeviceNodeState>> = SyncLazy::new(|| Mutex::new(DeviceNodeState::new()));
+
+pub fn allocate(pairs: &[(&str, ObjID)]) -> Vec<DeviceNode> {
+	let mut state = STATE.lock().unwrap();
+	pairs
+		.iter()
+		.map(|(rootname, id)| {
+			let num = if let Some(val) = state.rootcounters.get(*rootname) {
+				let val = *val;
+				state.rootcounters.insert(String::from(*rootname), val + 1);
+				val + 1
+			} else {
+				state.rootcounters.insert(String::from(*rootname), 0);
+				0
+			};
+			let name = format!("{}{}", rootname, num);
+			DeviceNode { name: name, id: *id }
+		})
+		.collect()
+}
+
+/*
+pub fn allocate_pty() -> (ObjID, ObjID) {
+	(0, 0)
+}
+*/
